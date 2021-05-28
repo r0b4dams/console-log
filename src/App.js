af1ae7466d9e08e7116adf56f8f5fb1d63624b1a
Aurora Duncan <aurorabrynn@gmail.com>
2021-05-28 10:52:07 -0700

    added dashboardcontainer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import './App.css';
 import HomeContainer from "./components/HomeContainer";
 import OneWalkContainer from "./components/OneWalkContainer";
+import DashboardContainer from "./components/DashboardContainer";
 
 function App() {
   return (
     <div className="App">
-      <OneWalkContainer />
+      <DashboardContainer />
     </div>
   );
 }
---
+++ src/components/DashboardContainer/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Home from "../_pages/Home";
+import NavBar from "../NavBar"
+import Footer from "../Footer"
+import GameGroup from "../GameGroup"
+import WalkGroup from "../WalkGroup"
+
+import games from "../games.json"
+import walkthroughs from "../walkthroughs.json"
+
+class HomeContainer extends Component {
+  state = {
+    games,
+    walkthroughs
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Home />
+
+        <div className="grid grid-flow-col">
+          <div className="col">
+            <GameGroup games={games}/>
+          </div>
+          <div className="col">
+            <WalkGroup walkthroughs={walkthroughs}/>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
