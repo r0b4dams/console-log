b7eb45fd724fd6683a6f23c44886ab8d08272bc5
Aurora Duncan <aurorabrynn@gmail.com>
2021-06-04 11:23:39 -0700

    messing with margins

--- src/components/_pages/HomePage.js
+++ src/components/_pages/HomePage.js
@@ -2,7 +2,7 @@ import HomeContainer from "../HomeContainer";
 
 function HomePage(props) {
   return (
-    <div className="p-2 bg-gray-200 bg-opacity-75 mb-2 mx-8 rounded border-2">
+    <div className="p-2 bg-gray-200 bg-opacity-75 mb-2 mx-48 rounded border-2">
       <h1 className="">{global.searchable && <div></div>}</h1>
       <HomeContainer />
     </div> 
