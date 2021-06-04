01ee8bfae733cf9bffdfc51c6e7901f8fa386550
Aurora Duncan <aurorabrynn@gmail.com>
2021-06-04 11:57:18 -0700

    finsihed styling

--- src/components/_pages/WalkPageAdd.js
+++ src/components/_pages/WalkPageAdd.js
@@ -74,7 +74,7 @@ function AddWalkPage({userState}) {
       </div>
     </div>
     <div>
-      <h1 className="text-4xl">
+      <h1 className="text-4xl text-white">
           Walkthrough by {userState.user.name}
       </h1>
     </div>
