9e821296c68e5ba3909ed4b94a64c30d72dc8782
Sung Kim <sungjk77@msn.com>
2021-06-06 03:08:53 -0700

    found a bug with genre

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -55,11 +55,11 @@ export default function NavBar(props) {
       <div className="text-right">
         <div className="grid grid grid-rows-1 grid-flow-col">
           <span className="flex items-center pl-0">
-            <div className="flex-none">
+            <div className="flex-none pt-1">
               {/* Click here to start speech-to-text */}
               {/* Button will not render if browser does not support web speech api */}
               {SpeechRecognition.browserSupportsSpeechRecognition() &&
-                <button onClick={SpeechRecognition.startListening} className="button">
+                <button onClick={SpeechRecognition.startListening} className="button p1">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
                   </svg>
