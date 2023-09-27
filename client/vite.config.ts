319fa3d056d1ce86c51130044d87bcc00ed810b8
Robert Adams <adamsiii.robert@gmail.com>
2023-09-26 21:04:49 -0700

    init vite client

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
