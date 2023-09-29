358192b7d2e2c8bd0b2a203e797ebbdde5537d00
Robert Adams <adamsiii.robert@gmail.com>
2023-09-28 22:55:12 -0700

    update with nexus schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,20 @@
 import http from 'http';
 import express from 'express';
-import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
 import { CONFIG } from './config';
-import { resolvers, typeDefs } from './gql';
+import { schema } from './schema';
 import { context } from './middleware/context';
-import type { AppContext } from './@types';
+import type { AppContext } from './@types/common';
 
 (async function main() {
   const app = express();
   const httpServer = http.createServer(app);
 
   const apolloServer = new ApolloServer<AppContext>({
-    typeDefs,
-    resolvers,
+    schema,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
@@ -26,7 +25,7 @@ import type { AppContext } from './@types';
   app.use('/graphql', expressMiddleware(apolloServer, { context }));
 
   httpServer.listen({ port: CONFIG.PORT }, () => {
-    console.log(`Server listening at http://localhost:${CONFIG.PORT} 🚀`);
+    console.log(`Server listening at http://localhost:${CONFIG.PORT}`);
     console.log(`Apollo Sandbox ready at http://localhost:${CONFIG.PORT}/graphql`);
   });
 })();
