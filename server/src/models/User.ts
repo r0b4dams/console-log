2192a0e0089ef14e4bfdc09d73b38401dc9125cd
Robert Adams <adamsiii.robert@gmail.com>
2023-09-29 00:17:04 -0700

    rm schema options

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,26 +1,20 @@
 import { Schema, model } from 'mongoose';
 import { NexusGenObjects as Type } from '../@types/schema';
 
-const userSchema = new Schema<Type['User']>(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    avatar: {
-      type: String,
-    },
+const userSchema = new Schema<Type['User']>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  {
-    _id: false,
-    versionKey: false,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-);
+  avatar: {
+    type: String,
+  },
+});
 
 export const User = model<Type['User']>('User', userSchema);
