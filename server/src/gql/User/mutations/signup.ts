0a5573712379cef68726d0a420f0d6fdf24c41f8
Robert Adams <adamsiii.robert@gmail.com>
2023-10-02 22:31:11 -0700

    rename signup with create

--- server/src/gql/User/mutations/signup.ts
+++ server/src/gql/User/mutations/signup.ts
@@ -1,7 +1,7 @@
 import { extendType, inputObjectType } from 'nexus';
 import { User } from '../../../models/User';
 
-const createUserInput = inputObjectType({
+const signupUserInput = inputObjectType({
   name: 'createUserInput',
   definition: (t) => {
     t.nonNull.string('username');
@@ -9,12 +9,12 @@ const createUserInput = inputObjectType({
   },
 });
 
-export const createUser = extendType({
+export const signupUser = extendType({
   type: 'Mutation',
   definition: (t) => {
     t.field('createUser', {
       type: 'User',
-      args: { data: createUserInput },
+      args: { data: signupUserInput },
       resolve: async (_, args) => await User.create({ ...args.data }),
     });
   },
